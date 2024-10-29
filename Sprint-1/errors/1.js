e4c96fc0bbec8abfff13dabd88668bfaf974313e
Della-Bella <gisdellabella@uol.com.br>
2024-10-29 23:07:47 +0000

    // ERROR=  Constant is a Variable can not be change so " const age = 33;" has to be set to "let" or "var"
    // ERROR=  Constant is a Variable that can not be changed so " const age = 33;" has to be set to "let" or "var"

--- Sprint-1/errors/1.js
+++ Sprint-1/errors/1.js
@@ -1,4 +1,7 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
-age = age + 1;
+// ERROR=  Constant is a Variable can not be change so " const age = 33;" has to be set to "let" or "var"
+
+let age= 33
+age= age + 1
+console.log ( age + 1 );
