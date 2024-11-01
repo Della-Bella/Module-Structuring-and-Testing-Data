39ac9bbe09da0508128f55745b593676a7b0a64b
Della-Bella <gisdellabella@uol.com.br>
2024-11-01 22:00:37 +0000

    modify ansewr excercise 0.js

--- Sprint-1/errors/3.js
+++ Sprint-1/errors/3.js
@@ -7,3 +7,11 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+const cardNumber = 4533787178994213;
+const last4Digits = cardNumber.slice(-4);
+
+function cardnumber () {
+    cardNumber.slice(-4)
+}
+last4Digitsn()
