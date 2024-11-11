fe4ebe88336ec9cf78727f9b7bda2423bbc0159b
Della-Bella <gisdellabella@uol.com.br>
2024-11-11 18:47:19 +0000

    Debug error line "return"

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,21 @@
 // Predict and explain first...
 
+/*It will print a error as return is in one line and Javasript will Not
+read any code after that line
+
+
 function sum(a, b) {
   return;
   a + b;
 }
 
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);*/
+
+//FIX://
+
+function sum(a, b) {
+  return; a + b;
+}
+
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
