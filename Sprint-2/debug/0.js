c36d5b3c106f4c528b5fe9e48d495b2d783a04d1
Della-Bella <gisdellabella@uol.com.br>
2024-11-11 18:46:41 +0000

    Debug exercise 0.js undefined function values

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,7 +1,25 @@
 // Predict and explain first...
 
+/*it will  print 
+the first console.log (a * b) which will call the function inputs 10, 32 = 320
+
+Then give a error/ undefined because .
+
 function multiply(a, b) {
   console.log(a * b);
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+DEBUG*/
+
+//The result of multiplying 10 and 32 is 320
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+
+
