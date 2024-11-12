5e34bb57c11653344e13057527c154e805c075b6
Della-Bella <gisdellabella@uol.com.br>
2024-11-12 08:20:50 +0000

    SyntaxError: Identifier 'str' has already been declared/ defined correct the function

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -3,7 +3,23 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
+/*function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+
+Error: SyntaxError: Identifier 'str' has already been declared
+
+Fix:*/
+
+function capitalise(capital) {
+  let str = `${capital.toUpperCase()}${capital.slice(1)}`;
+  return str;
+}
+
+console.log(`${capitalise(" change this text to uppercase " )}`);
+
+
+/*gisdellabella@Giss-MBP errors % node 0.js
+ CHANGE THIS TEXT TO UPPERCASE change this text to uppercase 
+gisdellabella@Giss-MBP errors %*/
