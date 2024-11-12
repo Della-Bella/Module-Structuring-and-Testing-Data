03b3c5e718058041c9c470c93ccf8dedb7409e33
Della-Bella <gisdellabella@uol.com.br>
2024-11-12 07:32:31 +0000

    Debug 2.js.The function don't have a parameter to receive the variable num

--- Sprint-2/debug/2.js
+++ Sprint-2/debug/2.js
@@ -10,5 +10,41 @@ console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
-// This program should tell the user the last digit of each number.
-// Explain why getLastDigit is not working properly - correct the problem
+/* This program should tell the user the last digit of each number.
+ the function doesn’t have a parameter to receive the num variable it’s trying to process. 
+// Explain why getLastDigit is not working properly - correct the problem 
+function getLastDigit() {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+
+
+function getLastDigit() {
+  return num.toString().slice(-1);
+}*/
+// FIX
+
+const num = 103;
+
+function getLastDigit() {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+function getLastDigit() {
+  return num.toString().slice(-1);
+}
+
+gisdellabella@Giss-MBP debug % node 2.js
+The last digit of 42 is 3
+The last digit of 105 is 3
+The last digit of 806 is 3
+42
+gisdellabella@Giss-MBP debug % 
