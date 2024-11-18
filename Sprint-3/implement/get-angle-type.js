e2de22f4f8ad051d7d254a33f40ad9188dc94847
Della-Bella <gisdellabella@uol.com.br>
2024-11-18 08:22:55 +0000

    Tests GetAngleType donne

--- Sprint-3/implement/get-angle-type.js
+++ Sprint-3/implement/get-angle-type.js
@@ -3,8 +3,21 @@
 function getAngleType(degrees) {
   if (degrees === 90) {
     return "Right angle";
-  } else if (degrees < 90) {
+
+  } else if (degrees > 0 && degrees < 90) {
     return "Acute angle";
+
+  } else if (degrees > 90 && degrees < 180) {
+    return "Obtuse angle";
+
+  } else if (degrees === 180) {
+    return "Straight angle";
+
+  } else if (degrees > 180 && degrees < 360) {
+    return "Reflex angle";
+    
+  } else {
+    return "Invalid angle"; // Handles cases where degrees are not in the valid range
   }
 }
 
--- Sprint-3/implement/getAngleType.test.js
+++ Sprint-3/implement/getAngleType.test.js
@@ -16,3 +16,34 @@ test(`should return "Acute angle" when degrees is less than 90 degrees`, functio
   expect(currentInput).toBe(targetInput);
 });
 
+// Identify Obtuse Angles:
+// When the angle is greater than 90 degrees and less than 180 degrees,
+// Then the function should return "Obtuse angle"
+//The angle 179° is a Obtuse angle
+
+test(`Should return Obtuse Angles when angle is greater than 90 degrees and less than 180 degrees`, function () {
+  const currentInput = getAngleType(120);
+  const targetInput = "Obtuse angle";
+  expect(currentInput).toBe(targetInput);
+});
+
+// Identify Straight Angles:
+// When the angle is exactly 180 degrees,
+// Then the function should return "Straight angle"
+//The angle 180° is a Straight angle.
+
+test(`should return Straight Angles when the angle is exactly 180 degree`, function () {
+  const currentInput = getAngleType(180);
+  const targetInput = "Straight angle";
+  expect(currentInput).toBe(targetInput);
+});
+
+// Identify Reflex Angles:
+// When the angle is greater than 180 degrees and less than 360 degrees,
+// Then the function should return "Reflex angle"
+
+test(`Should return Reflex Angles when the angle is greater than 180 degrees and less than 360 degrees`, function () {
+  const currentInput = getAngleType(190);
+  const targetInput = "Reflex angle";
+  expect(currentInput).toBe(targetInput);
+});
