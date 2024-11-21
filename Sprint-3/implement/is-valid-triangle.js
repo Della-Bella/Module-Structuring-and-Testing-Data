14e373bb0c591bf4f53f5fe51edf0f23b341e980
Della-Bella <gisdellabella@uol.com.br>
2024-11-21 20:43:29 +0000

    Exercise Triangle Check Sides

--- Sprint-3/implement/is-valid-triangle.js
+++ Sprint-3/implement/is-valid-triangle.js
@@ -1,4 +1,31 @@
 // Implement a function isValidTriangle
+//console.log(isValidTriangle(1, 6, 5));
+//console.log(isValidTriangle(1, 0, 3));
+
+function isValidTriangle(a, b, c) {
+  if (a <= 0 || b <= 0 || c <= 0) {
+    return false; 
+  }
+
+  //sum of any two sides must be greater than the third)
+  if (a + b <= c || a + c <= b || b + c <= a) {
+    return false; // Return false 
+  }
+  return true;
+}
+
+module.exports = isValidTriangle;
+
+// first i put && and it gave undefined, so I used | | which checks checks if at least
+    // one condition is true.It returns true if any of the conditions is true.
+    //It only returns false if all conditions are false.false, 0, null, undefined, NaN,
+
+// Example usage
+//console.log(isValidTriangle(1, 6, 5)); // Outputit is not valid triangle
+//console.log(isValidTriangle(1, 0, 3)); // Output: it is not valid triangle
+
+
+
 // Terms
 // the Triangle Inequality says: the sum of any two sides is always greater than the third side.
 // practical examples:
@@ -11,15 +38,20 @@
 // It's also true that b + c > a
 // It's also true that a + c > b
 
-// In our function isValidTriangle which takes as parameters the lengths of three sides, we need to invalidate any triangle where the sum of any two sides is less than or equal to the length of the third side.
-// and we need to validate any triangle where the sum of any two sides is greater than the length of the third side.
+// In our function isValidTriangle which takes as parameters the lengths of three sides,
+//we need to invalidate any triangle where the sum of any two sides is less than or equal to
+//the length of the third side.
+// and we need to validate any triangle where the sum of any two sides is greater than the
+// length of the third side.
 
 // Acceptance criteria:
 
 // scenario: invalid triangle
 // Given the side lengths a, b, and c,
-// When the sum of any two side lengths is less than or equal to the length of the third side (i.e., a + b <= c, a + c <= b, b + c <= a),
-// Then it should return false because these conditions violate the Triangle Inequality, which states that the sum of the lengths of any two sides of a triangle must be greater than the length of the third side.
+// When the sum of any two side lengths is less than or equal to the length of the third side
+//(i.e., a + b <= c, a + c <= b, b + c <= a),
+// Then it should return false because these conditions violate the Triangle Inequality,
+// which states that the sum of the lengths of any two sides of a triangle must be greater than the length of the third side.
 
 // scenario: invalid triangle
 // Check for Valid Input:
@@ -32,4 +64,6 @@
 // When the function is called with these values as input,
 // Then it should return true because the input forms a valid triangle.
 
-// This specification outlines the behavior of the isValidTriangle function for different input scenarios, ensuring it properly checks for invalid side lengths and whether they form a valid triangle according to the Triangle Inequality Theorem.
+// This specification outlines the behavior of the isValidTriangle function for different input scenarios, ensuring it properly checks for invalid side lengths and whether they form a valid triangle according to the Triangle Inequality Theorem.*/
+
+
