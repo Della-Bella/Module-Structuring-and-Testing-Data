b5488f5aa53ffd75375f9af3d1f77345372225e9
Della-Bella <gisdellabella@uol.com.br>
2024-11-22 20:54:37 +0000

    fixed link test name

---
+++ Sprint-3/revise/implement/get-ordinal-number.js
@@ -0,0 +1,32 @@
+// In this week's prep, we started implementing getOrdinalNumber
+
+// continue testing and implementing getOrdinalNumber for additional cases
+// Write your tests using Jest - remember to run your tests often for continual feedback
+
+function getOrdinalNumber(number) {
+  const suffixes = ["th", "st", "nd", "rd"];
+  const suffixIndex = number % 10;
+
+  if (suffixIndex === 1 && number !== 11) {
+    return number + "st";
+  } else if (suffixIndex === 2 && number !== 12) {
+    return number + "nd";
+  } else if (suffixIndex === 3 && number !== 13) {
+    return number + "rd";
+  } else {
+    return number + "th";
+  }
+}
+
+//const input = 1;
+//const input = 4;
+//4 as an ordinal number will be 4th
+//const input = 12;
+//12 as an ordinal number will be 12th
+const ordinalNumber = getOrdinalNumber(input);
+console.log(`1 as an ordinal number will be ${ordinalNumber}`);
+
+module.exports = getOrdinalNumber;
+
+
+
--- Sprint-3/revise/implement/get-ordinal-number.test.js
+++ Sprint-3/revise/implement/get-ordinal-number.test.js
@@ -1,15 +1,8 @@
-// In this week's prep, we started implementing getOrdinalNumber
 
-// continue testing and implementing getOrdinalNumber for additional cases
-// Write your tests using Jest - remember to run your tests often for continual feedback
 
-const input = 1;
+const getOrdinalNumber = require("./getordinalnumber");
 
-function getOrdinalNumber() {
-  const currentOutput = getOrdinalNumber(input);
-  const targetOutput = "1st";
-  return const currentOutput = getOrdinalNumber(input);
-}
-const currentOutput = getOrdinalNumber(input);
-console.log(`1 as an ordinal number will be ${getOrdinalNumber()}`);
 
+test("converts 1 to an ordinal number", function () {
+  expect(getOrdinalNumber(1)).toEqual("1st");
+});
