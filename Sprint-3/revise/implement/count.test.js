632a7155d02be68822c8e4f849990d2f93814b95
Della-Bella <gisdellabella@uol.com.br>
2024-11-22 07:18:22 +0000

    implement and test Rotate.char Exercise

--- Sprint-3/revise/implement/count.test.js
+++ Sprint-3/revise/implement/count.test.js
@@ -1,7 +1,29 @@
-// implement a function countChar that counts the number of times a character 
+// implement a function countChar that counts the number of times a 
+//character 
 //occurs in a string
 
+function countChar(str, char) {
+  let count = 0;
 
+  // Iterate over each character in the string
+  for (let c of str) {
+    if (c === char) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+// Scenario: Multiple Occurrences
+const testString1 = "aaaaa";
+const testChar1 = 'a';
+console.log(countChar(testString1, testChar1)); // Output: 5
+
+// Scenario: No Occurrences
+const testString2 = "paralelepipedo";
+const testChar2 = 'z';
+console.log(countChar(testString2, testChar2)); // Output: 0
 
 
 // Given a string str and a single character char to search for,
@@ -10,9 +32,11 @@
 
 // Scenario: Multiple Occurrences
 // Given the input string str,
-// And a character char that may occur multiple times with overlaps within str (e.g., 'a' in 'aaaaa'),
+// And a character char that may occur multiple times with overlaps
+// within str (e.g., 'a' in 'aaaaa'),
 // When the function is called with these inputs,
-// Then it should correctly count overlapping occurrences of char (e.g., 'a' appears five times in 'aaaaa').
+// Then it should correctly count overlapping occurrences of char
+// (e.g., 'a' appears five times in 'aaaaa').
 
 // Scenario: No Occurrences
 // Given the input string str,
--- Sprint-3/revise/investigate/find.js
+++ Sprint-3/revise/investigate/find.js
@@ -13,10 +13,12 @@ function find(str, char) {
 console.log(find("code your future", "u"));
 console.log(find("code your future", "z"));
 
-// The while loop statement allows us to do iteration - the repetition of a certain number of tasks according to some condition
+// The while loop statement allows us to do iteration - the repetition of a certain number of tasks
+// according to some condition
 // See the docs https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while
 
-// Use the Python Visualiser to help you play computer with this example and observe how this code is executed
+// Use the Python Visualiser to help you play computer with this example and observe how this 
+//code is executed
 // Pay particular attention to the following:
 
 // a) How the index variable updates during the call to find
