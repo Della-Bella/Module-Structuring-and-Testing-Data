661fd6d1f47b84ad1a2f41f56be3f615781a7ed3
Della-Bella <gisdellabella@uol.com.br>
2024-12-21 12:14:33 +0000

    Fixing function returning the Sum

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -14,8 +14,11 @@ console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);*/
 //FIX://
 
 function sum(a, b) {
-  return; a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+//The result wasn't returned because the sum of a and b wasn't included.
+
 
