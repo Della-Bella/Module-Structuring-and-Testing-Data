8dd6ea58449209d54ac69a99c6bbac45ebdc842e
Della Bella <gisdellabella@uol.com.br>
2025-10-21 19:40:01 +0100

    Test Count

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -22,3 +22,9 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+test("should countthe ocorencies and return empty",()=>{
+  const str = "abcde";
+  const char = "f";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
